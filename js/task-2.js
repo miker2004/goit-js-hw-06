d800d53b4cbeefeeb0261e454440502736fa793c
miker2004 <157730940+miker2004@users.noreply.github.com>
2024-06-07 15:59:36 +0200

    obby dobrze

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+class Storage {
+
+    constructor (items) {
+        this.items = items;
+    };
+
+    getItems(){
+        return this.items
+    };
+
+    addItem(newItem) {
+        this.items.push(newItem)
+    };
+
+    removeItem(itemToRemove) {
+        const idx = this.items.indexOf(itemToRemove);
+        if (idx !== -1 ) this.items.splice(idx, 1);
+    };
+};
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
